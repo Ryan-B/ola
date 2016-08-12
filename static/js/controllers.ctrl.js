e729c84b2fcd4417790943e71d9b438491646405
Ryan Bird <dawgillac11@yahoo.com>
2016-08-12 00:46:03 -0700

    pro image add2

--- static/js/controllers.ctrl.js
+++ static/js/controllers.ctrl.js
@@ -13,7 +13,7 @@ app.controller('HomeController', function($interval, $scope) {
 	console.log("HomeController Loaded")
 
 		var dimages= [];
-		var numImages=11;
+		var numImages=13;
 
 		for (i=0; i<numImages; i++) {
 		  // dimages[i]=new Image();
@@ -30,7 +30,7 @@ app.controller('HomeController', function($interval, $scope) {
 				k = 0;
 			}
 			this.image = dimages[k];
-		}.bind(this), 8000);
+		}.bind(this), 6000);
 
 	$scope.$on('$destroy', function() {
 		$interval.cancel(intervalId);
